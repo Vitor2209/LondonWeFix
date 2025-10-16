cf6e475e713ae2496b1b205b4424dd9e82195f33
Vitor Dutra Melo <vitordutra1125@gmail.com>
2025-10-16 12:08:12 +0100

    update

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// Torna o botão clicado "ativo" e reseta os outros
+document.querySelectorAll('.nav-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
